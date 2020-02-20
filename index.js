2864636730bae58bdd4618cba9015040c129acf4
WanderSmits <wandersmits@gmail.com>
2020-02-20 15:10:59 +0100

    ignore node_modules

---
+++ index.js
@@ -0,0 +1,27 @@
+//import express package
+const express = require("express");
+//create new server
+const app = express();
+//define basic html document
+const page = `<hmtl>
+  <head>
+    <title>Wander</title>
+  </head>
+  <body>
+    <h1>Why do comedians love !false?</h1>
+    <p>It's funny because it's true.</p>
+  </body> 
+</html>`;
+
+//define GET / endpoint
+app.get("/", (request, response) => {
+  response.send(page);
+});
+
+//set port to 3000
+const port = 3000;
+
+//makes the app go to localhost on port 3000
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
